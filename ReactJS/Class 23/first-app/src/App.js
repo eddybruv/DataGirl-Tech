63a96efe96c65457a84e6e863107916d81d24ce8
eddybruv <edwinajong@gmail.com>
2022-03-18 19:33:59 +0100

    react practice

---
+++ ReactJS/Class 23/first-app/src/App.js
@@ -0,0 +1,8 @@
+import Name from './components/Name.jsx';
+import style from './App.module.css';
+
+function App () {
+  return <p className={style.yaya}>Hello <Name/>♥</p>;
+}
+
+export default App;
---
+++ ReactJS/Class 23/first-app/src/components/Name.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../App.module.css'
+
+function Name() {
+  return <span> Edwiin</span>;
+}
+
+export default Name
