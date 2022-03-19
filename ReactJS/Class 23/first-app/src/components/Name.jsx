f392589bf13c4e00e38191e76959c930078239b5
eddybruv <edwinajong@gmail.com>
2022-03-19 17:45:41 +0100

    learned css styling in react

--- ReactJS/Class 23/first-app/src/components/Name.jsx
+++ ReactJS/Class 23/first-app/src/components/Name.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
-import '../App.module.css'
+import React from "react";
+import "../App.module.css";
 
 function Name() {
-  return <span> Edwiin</span>;
+  const redText = {
+    fontSize: '2rem',
+    color: 'red'
+  }
+  let people = [
+    { firstName: "Eddy", color: "Chocolate", age: 20 },
+    { firstName: "Sonia", color: "Light skin", age: 18 },
+    { firstName: "Kyle", color: "Light skin", age: "unborn" },
+  ];
+
+  const listPeople = people.map((person) => {
+    return (
+      <div>
+        <span style={redText}>First Name: {person.firstName}</span>
+        {" "}
+        Complexion: {person.color} Age: {person.age}
+      </div>
+    );
+  });
+
+  return <div>{listPeople}</div>;
 }
 
-export default Name
+export default Name;
