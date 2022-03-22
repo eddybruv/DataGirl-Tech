7ee095979457587ca0f834192fa4215f7efc6165
eddybruv <edwinajong@gmail.com>
2022-03-22 21:35:45 +0100

    changed to jsx file

---
+++ ReactJS/Class 23/first-app/src/App.jsx
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from 'react'
+import {Age} from './components/Age'
+
+function App () {
+
+  let [counter, setCounter] = useState(0);
+  let [first, setFirst] = useState(5)
+
+  useEffect(() => {
+    counter > 0 && setFirst(first + 1);
+  }, [counter])
+
+  const changeCounter = (option) => {
+    option == 'decrement' && setCounter(counter - 1);
+    option == "increment" && setCounter(counter + 1);
+  }
+
+  return (
+    <div style={{ margin: "3rem", textAlign: "center" }}>
+      <h1>First = {first}</h1>
+      {/* <h3>{counter}</h3> */}
+      <button onClick={() => changeCounter("increment")}>Increment</button>
+
+      <Age age={counter} changeCounter={changeCounter}/>
+    </div>
+  );
+}
+
+export default App;
