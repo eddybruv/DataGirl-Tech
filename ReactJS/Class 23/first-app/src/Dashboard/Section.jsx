76e77062dcbe87712af6220bcfe428e60efbff90
eddybruv <edwinajong@gmail.com>
2022-03-24 19:29:11 +0100

    section component

---
+++ ReactJS/Class 23/first-app/src/Dashboard/Section.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import instagram from './download.jpeg'
+import Card from './Card'
+
+function Section({columns}) {
+  const list = [
+    { img: instagram, firstText: "First text", secondText: "second text" },
+    { img: instagram, firstText: "First text", secondText: "second text" },
+    { img: instagram, firstText: "First text", secondText: "second text" },
+    { img: instagram, firstText: "First text", secondText: "second text" },
+    { img: instagram, firstText: "First text", secondText: "second text" },
+    { img: instagram, firstText: "First text", secondText: "second text" },
+    { img: instagram, firstText: "First text", secondText: "second text" },
+    { img: instagram, firstText: "First text", secondText: "second text" },
+    { img: instagram, firstText: "First text", secondText: "second text" },
+    { img: instagram, firstText: "First text", secondText: "second text" },
+    { img: instagram, firstText: "First text", secondText: "second text" },
+    { img: instagram, firstText: "First text", secondText: "second text" },
+  ];
+
+  const listCards = list.map((item, index) => {
+    return <Card item={item} key={index}/>
+  })
+
+  return (
+    <div className="section-container">
+      <div className="left-section">Left Section </div>
+      <div style={{gridTemplateColumns: `repeat(${columns}, 1fr)`}} className="right-section">
+        {listCards}
+      </div>
+    </div>
+  );
+}
+
+export default Section
