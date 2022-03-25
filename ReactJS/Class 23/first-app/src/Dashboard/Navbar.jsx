9828090b9767fac06e1a992c55345cd2b47eb955
eddybruv <edwinajong@gmail.com>
2022-03-25 22:20:42 +0100

    dashboard's navbar

---
+++ ReactJS/Class 23/first-app/src/Dashboard/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Navbar() {
+
+  return (
+    <>
+      <div className="nav-container">
+        <div className="logo"></div>
+
+        <div className="nav-list">
+          <div>Main feed</div>
+          <div>Stream</div>
+          <div>Settings</div>
+        </div>
+
+        <div className="logo"></div>
+      </div>
+
+      <div className="blue-section">
+        Hello there
+      </div>
+    </>
+  );
+}
+
+export default Navbar
