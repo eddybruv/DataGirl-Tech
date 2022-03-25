77f5b01cf72fa6f2a88974fa942a5827b74fe47a
eddybruv <edwinajong@gmail.com>
2022-03-25 22:24:07 +0100

    Lower section component

---
+++ ReactJS/Class 23/login-ui/src/Components/SectionTwo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Form from './Form'
+
+function SectionTwo() {
+
+  return (
+    <div className='section-two'>
+      <Form />
+    </div>
+  )
+}
+
+export default SectionTwo
