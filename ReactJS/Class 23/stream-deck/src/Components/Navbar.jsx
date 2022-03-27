5582c8174a9e170a27fa57aeac03f47921737585
eddybruv <edwinajong@gmail.com>
2022-03-27 21:29:55 +0100

    to finish

---
+++ ReactJS/Class 23/stream-deck/src/Components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faSquare } from "@fortawesome/free-solid-svg-icons";
+
+function Navbar() {
+  
+}
+
+export default Navbar
