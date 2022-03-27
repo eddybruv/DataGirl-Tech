ec90f8619148883ecd55dd121bb41080e5ffa1f2
eddybruv <edwinajong@gmail.com>
2022-03-27 23:22:55 +0100

    done with nav bar

--- ReactJS/Class 23/stream-deck/src/Components/Navbar.jsx
+++ ReactJS/Class 23/stream-deck/src/Components/Navbar.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import { faSquare } from "@fortawesome/free-solid-svg-icons";
+import { faCaretRight, faCodeFork, faDoorClosed, faFeed, faScrewdriver, faSquareXmark } from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
-  
+  return (
+    <>
+      <div className="navbar">
+        <div>
+          <FontAwesomeIcon className="icon square" icon={faSquareXmark} />
+        </div>
+        <div className="rest">
+          <div className="nav-list">
+            <ul>
+              <li>
+                <FontAwesomeIcon className="icon" icon={faFeed} /> Main Feed
+              </li>
+              <li>
+                <FontAwesomeIcon className="icon" icon={faCodeFork} /> Stream
+              </li>
+              <li>
+                <FontAwesomeIcon className="icon" icon={faScrewdriver} />{" "}
+                Settings
+              </li>
+            </ul>
+          </div>
+          <div className="logout">
+            <FontAwesomeIcon icon={faDoorClosed} />
+          </div>
+        </div>
+      </div>
+      <div className="lower-bar">
+        <p>Activity Stream <FontAwesomeIcon className='carret' icon={faCaretRight} /> <span className="blue-text">List of items</span></p>
+      </div>
+    </>
+  );
 }
 
 export default Navbar
