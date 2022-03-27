ef29edd051ffde15a57434dc3364550b7f4d1e26
eddybruv <edwinajong@gmail.com>
2022-03-27 23:22:44 +0100

    done with card component

--- ReactJS/Class 23/stream-deck/src/Components/Card.jsx
+++ ReactJS/Class 23/stream-deck/src/Components/Card.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretRight } from "@fortawesome/free-solid-svg-icons";
 
-function Card() {
+function Card({item}) {
   return (
-    <div>
-      
+    <div className="card" >
+      <img src={item.img} alt="" />
+      <div className="text">
+        <p className="title">{item.title}</p>
+        <p className="desc">{item.desc}</p>
+      </div>
+      <FontAwesomeIcon className='caret' icon={faCaretRight} />
     </div>
-  )
+  );
 }
 
 export default Card
