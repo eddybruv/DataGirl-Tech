e1668a39660faf0cd9098789b05b2d2b2852e460
eddybruv <edwinajong@gmail.com>
2022-03-28 01:37:51 +0100

    check later

--- Class 19/Basic exercises/68.js
+++ Class 19/Basic exercises/68.js
@@ -1,2 +1,13 @@
 //Write a program to shift an element in left direction and return a new array
 
+let arr = [10, 20, 30, 40];
+
+let swap = (arr) => {
+  let newArr = [...arr];
+  let value = newArr[0];
+  newArr.splice(0, 1);
+  newArr.push(value);
+  return newArr;
+}
+
+console.log(swap(arr));
