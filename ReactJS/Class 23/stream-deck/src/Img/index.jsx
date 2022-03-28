b4a287edca12796690d5e843b4090c94a441263c
eddybruv <edwinajong@gmail.com>
2022-03-28 01:32:52 +0100

    photo import/export

---
+++ ReactJS/Class 23/stream-deck/src/Img/index.jsx
@@ -0,0 +1,26 @@
+import instagram from "./../Img/instagram.png";
+import spotify from "./../Img/spotify.png";
+import twitter from "./../Img/twitter.png";
+import adword from "./../Img/adword.png";
+import apple from '../Img/apple.png'
+import pinterest from "../Img/pin.png";
+import nike from "../Img/nike.png";
+import skype from "../Img/skype.png";
+import tum from "../Img/tum.png";
+import slack from "../Img/slack.png";
+import citymapper from '../Img/citymapper.png'
+
+// second section images
+import opera from "../Img/opera.png";
+import chrome from "../Img/chrome.png";
+import firefox from "../Img/firefox.png";
+import safari from "../Img/safari.png";
+import adobe from "../Img/adobe.png";
+import itunes from "../Img/itunes.png";
+
+export {instagram, spotify, twitter, adword, apple, pinterest, nike, skype, tum, slack, citymapper} 
+
+// second section export 
+export {
+  opera, chrome, firefox, safari, adobe, itunes
+}
