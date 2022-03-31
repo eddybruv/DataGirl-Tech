866830edc42dbb79bf2d8ebd2723c506e6a080d8
eddybruv <edwinajong@gmail.com>
2022-03-31 21:28:32 +0100

    added collapsible

--- ReactJS/Class 23/service-firm/src/Components/LowerCard.jsx
+++ ReactJS/Class 23/service-firm/src/Components/LowerCard.jsx
@@ -1,42 +1,28 @@
 import React, { useState } from "react";
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faPlus} from '@fortawesome/free-solid-svg-icons' 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
-function LowerCard({item}) {
-  const [show, setShow] = useState(false);
-  let paragraph = document.getElementsByClassName('hide-text');
-  console.log("🚀 ~ file: LowerCard.jsx ~ line 8 ~ LowerCard ~ paragraph", paragraph)
-  
-  const handleShow = () => {
-    if(!show) {
-      setShow(true);
-      paragraph.classList.remove('hide-text');
-    } else {
-      setShow(false);
-      paragraph.classList.add('hide-text');
-    }
-  }
+function LowerCard({ item }) {
+  const [show, setShow] = useState(true);
 
-
-  // let showW = document.getElementByClassName("hide-text");
-  
-  // let i;
-
-  // for(i = 0; i < showW.length; i++){
-  //   showW[i].addEventListener("click", function(){
-  //     this.classList.toggle("new_classname");
-  //   })
-  // }
+  const handleShow = (event) => {
+    setShow(!show);
+    console.log(show);
+  };
 
   return (
     <div className="lower-card">
       <div className="header-icon">
-        <p>{item.title}</p>
-        <FontAwesomeIcon onClick={handleShow} className="plus-icon" icon={faPlus} />
+        <p className="title">{item.title}</p>
+        <FontAwesomeIcon
+          onClick={handleShow}
+          className="plus-icon"
+          icon={faPlus}
+        />
       </div>
-      <p className="hide-text">{item.text}</p>
+      {!show && <p className="p-text">{item.text}</p>}
     </div>
   );
 }
 
-export default LowerCard
+export default LowerCard;
