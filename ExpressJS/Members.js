719fcd3acaad5e10c66e782d59e038ba7591bea2
eddybruv <edwinajong@gmail.com>
2022-04-01 20:08:54 +0100

    array of members

---
+++ ExpressJS/Members.js
@@ -0,0 +1,34 @@
+const members = [
+  {
+    id: 1,
+    name: "Sonia A",
+    email: "sonia@gmail.com",
+    status: "inactive",
+  },
+  {
+    id: 2,
+    name: "Edwin A",
+    email: "edwin@gmail.com",
+    status: "active",
+  },
+  {
+    id: 3,
+    name: "Kyle A",
+    email: "sonia@gmail.com",
+    status: "active",
+  },
+  {
+    id: 4,
+    name: "mummy A",
+    email: "mummy@gmail.com",
+    status: "active",
+  },
+  {
+    id: 5,
+    name: "daddy A",
+    email: "daddy@gmail.com",
+    status: "inactive",
+  },
+];
+
+module.exports = members;
