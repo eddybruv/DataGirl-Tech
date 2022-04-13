c0761cf7295f92d78e91e8c3bbf0321d857d69fc
eddybruv <edwinajong@gmail.com>
2022-04-13 21:14:47 +0100

    added post routers

--- ExpressJS/Comments/server.js
+++ ExpressJS/Comments/server.js
@@ -6,10 +6,12 @@ const connection = mongoose.connect(
   "mongodb+srv://eddybruv:bimelaA4@shared-cluster.l6far.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 );
 const UserRouter = require('./routes/User.router')
+const PostRouter = require('./routes/Post.router');
 
 app.use(express.json());
 app.use(express.urlencoded());
 app.use('/api/user', UserRouter);
+app.use('/api/post', PostRouter);
 
 connection.then(() => console.log('Connection successful'));
 connection.catch(() => console.log('connection unsuccessful'));
